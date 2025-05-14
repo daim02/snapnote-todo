754855d5935c98a92a32870063722b0a8e49050e
Daim Ahmed <190954303+daim02@users.noreply.github.com>
2025-05-14 14:38:33 +0100

    Update SupabaseClient.js
    Removed API keys (now reset to avoid any malicious intent) + using a .env file for variables.

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eshkpbjjmnojqnmojbhw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzaGtwYmpqbW5vanFubW9qYmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDg5NTksImV4cCI6MjA2MjcyNDk1OX0.Q6QdzTRrV6CRiy06wTMFLuK3PXUn05QKlKOIVcfKUwM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
